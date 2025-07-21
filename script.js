e9187cc56030060dcf45aa45e96530b18aa89cd0
myatmh <myatminhtwe7@gmail.com>
2025-07-22 04:32:45 +0630

    Refactor form structure and enhance styling for responsiveness

---
+++ script.js
@@ -0,0 +1,8 @@
+const apiKeys = "efff1ed95ca8f8a524e4bf6962f6312c";
+
+const inputTag = document.querySelector(".input_box");
+const submitBtn = document.querySelector(".submit_btn");
+
+//Logic
+
+//https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={APIkey}&units=metric
